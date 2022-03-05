02ab717bd1a1cd0d0541e13589db383266ca7272
EliasKyster <eliaskyster@gmail.com>
2022-03-05 10:00:08 +0100

    Initial Commit

---
+++ LearningGH_NEW/LearningGH_NEWApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGH_NEWApp.swift
+//  LearningGH_NEW
+//
+//  Created by Elias Kyster on 05/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGH_NEWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
